f7a489813e3ca5004de91744de7e1424eac6a43c
brandiw <brandimichelle.0911@gmail.com>
2018-05-08 11:01:51 -0700

    Sign up form implemented (no auth yet)

---
+++ client/src/Profile.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Profile extends Component {
+  render() {
+    return(
+        <p>This is a profile page. You must be logged in to see it.</p>
+      );
+  }
+}
+
+export default Profile;
